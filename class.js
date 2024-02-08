7aa2a86729d8613eea0420a30edecf607b220fab
LucasGabriel77 <lucasgrs01616@gmail.com>
2024-02-08 11:54:16 -0300

    versao final

--- class.js
+++ class.js
@@ -4,4 +4,25 @@ class eleitor{
         this.idade = idade 
         this.titulo = titulo
     }
+    verificavoto(){
+        let idade = this.idade
+        console.log(idade)
+
+        if (idade < 16) {
+            console.log("abaixo da idade minima para votar")
+            return "abaixo da idade minima para votar"
+
+        } else if((idade >= 16)&&(idade <18)){
+            console.log("voto facultativo")
+            return " voto facultativo"
+
+        } else if((idade>=18)&&(idade <= 70 )){
+            console.log("voto obrigatorio")
+            return " voto obrigatorio"
+
+        } else if (idade > 70){
+            console.log("voto facultativo")
+            return " voto facultativo"
+        }
+    }
 }
--- main.js
+++ main.js
@@ -14,5 +14,15 @@ verificar.addEventListener('click', ()=>{
     cidadao.titulo = titulo
     
     console.log("instancia do formulario",cidadao)
+
+   let mensagem = cidadao.verificavoto()
+
+   resposta.innerHTML = ''
+   resposta.innerHTML += `o eleitor ${cidadao.nome} <br>`
+   resposta.innerHTML += `tem ${cidadao.idade} anos de idade`
+   resposta.innerHTML += mensagem + `<br>`
+   resposta.style.color
+
+
 })
 
